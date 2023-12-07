5313623f3bbe21cc391221a6ac69d65ee310cb2e
jathin <jathingokarnam@gmail.com>
2023-12-07 14:36:44 +0530

    commited

---
+++ src/main/java/com/it/mavenjava/App.java
@@ -0,0 +1,13 @@
+package com.it.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
